de935bed72519995bef6f6aa5f027bb9fb1328ea
Michael Chadwick <michael.chadwick@gmail.com>
2018-02-06 00:16:20 -0800

    formatting

--- js/codaname-api.js
+++ js/codaname-api.js
@@ -1,26 +1,28 @@
 $(function() {
-	$.ajax({
-  	dataType: 'json',
-  	url: 'http://codana.me/pages.json',
-  	success: function (data) {
-			var entries = data.entries;
-	    var post = entries[entries.length-1];
-			var postDate = post.url.substr(1,10);
-			var postTitle = post.title;
-			var postUrl = "http://codana.me" + post.url;
-			postDate = replaceAll(postDate, '/', '-');
-			$('.blogCodaname').html('<span>Latest post: ' + postDate + '<br /><a href="' + postUrl + '">' + postTitle + '</a></span>');
-			if ($('.apiData').prop('display') != 'block') {
-				$('.apiData').show();
-			}
-	  }
-	});
+  $.ajax({
+    dataType: 'json',
+    url: 'http://codana.me/pages.json',
+    success: function (data) {
+      var entries = data.entries;
+      var post = entries[entries.length-1];
+      var postDate = post.url.substr(1,10);
+      var postTitle = post.title;
+      var postUrl = "http://codana.me" + post.url;
+      postDate = replaceAll(postDate, '/', '-');
 
-	function replaceAll(str, find, replace) {
-	  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
-	}
+      $('.blogCodaname').html('<span>Latest post: ' + postDate + '<br /><a href="' + postUrl + '">' + postTitle + '</a></span>');
 
-	function escapeRegExp(str) {
-  	return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-	}
+      if ($('.apiData').prop('display') != 'block') {
+        $('.apiData').show();
+      }
+    }
+  });
+
+  function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+  }
+
+  function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  }
 });
