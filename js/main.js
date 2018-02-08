a0bbe83d05dd8b2a5cbbc4c47eee496b043d86df
Michael Chadwick <mchadwick@ucsd.edu>
2018-02-08 08:05:56 -0800

    added light/dark theme switcher; moved ga to own file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-$(function(){
-  var favicon_src = "";
-  var $link = "";
-
-  $("ul.links li a[href^='http']").each(function() {
-    $(this).parent().css("list-style-image", "url('https://www.google.com/s2/favicons?domain=" + this.hostname + "')");
+$(function () {
+  $('ul.links li a[href^="http"]').each(function () {
+    $(this).parent().css(
+      'list-style-image', `url('https://www.google.com/s2/favicons?domain=${this.hostname}')`)
   });
-});
+  
+  $style = $('style#theme')
+  $('div#theme a.light').on('click', function () {
+    $style.html('@import url("css/theme/light.css");')
+  })
+  $('div#theme a.dark').on('click', function (link) {
+    $style.html('@import url("css/theme/dark.css");')
+  })
+})
