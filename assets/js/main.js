5ac1e787cdbc844e4012bff3411ff147a5135970
Michael Chadwick <mchadwick@ucsd.edu>
2020-12-23 14:30:41 -0800

    refactor favicon trick; added fench

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 $(function () {
-  $('ul.links li a[href^="http"]').each(function () {
+  // find links and change their <li> background to be their favicon
+  $('ul.links li a.favicon[href^="http"]').each(function () {
     $(this).parent().css(
-      'list-style-image', `url('https://www.google.com/s2/favicons?domain=${this.hostname}')`)
+      'list-style-image', `url('https://www.google.com/s2/favicons?domain=${this.hostname}')`
+    )
   })
 
+  // change the theme to day/night
   $style = $('style#theme')
   $('div#theme a.light').on('click', function () {
     $style.html('@import url("assets/css/theme/light.css")')
