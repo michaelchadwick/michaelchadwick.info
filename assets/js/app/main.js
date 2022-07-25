b5a2de987346432fb2c733bb59a061d06643e721
Michael Chadwick <mike@neb.host>
2022-07-25 12:04:15 -0700

    added a sticky header if on tablet or higher when you scroll down far enough

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -30,10 +30,11 @@ indentedLists.forEach(li => {
 })
 
 const btn = document.getElementById('theme-toggler')
+const header = document.querySelector('header')
+const headerScrolled = document.querySelector('header.header-scrolled')
 const bodyClasses = document.body.classList
 const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
 const currentTheme = localStorage.getItem('mcinfo-theme')
-
 
 if (currentTheme == 'dark') {
   bodyClasses.toggle('dark-theme')
@@ -43,29 +44,74 @@ if (currentTheme == 'dark') {
   btn.innerHTML = THEME_LIGHT_TEXT
 }
 
+let lastKnownScrollPosition = 0
+let ticking = false
+
+MCInfo.handleResize = () => {
+  headerScrolled.style.width = `${window.innerWidth - 20}px`
+
+  if (document.body.clientWidth < 768) {
+    headerScrolled.classList.remove('show')
+  }
+}
+
+MCInfo.handleScroll = () => {
+  lastKnownScrollPosition = window.scrollY
+
+  if (document.body.clientWidth >= 768) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event#examples
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        if (lastKnownScrollPosition > 200) {
+          headerScrolled.classList.add('show')
+        } else {
+          headerScrolled.classList.remove('show')
+        }
+
+        ticking = false
+      })
+
+      ticking = true
+    }
+  }
+}
+
+MCInfo.addEventHandlers = () => {
+  // Listen for a click on the button
+  btn.addEventListener('click', function(event) {
+    if (prefersDarkScheme.matches) {
+      bodyClasses.toggle('light-theme')
+      theme = bodyClasses.contains('light-theme') ? 'light' : 'dark'
+    } else {
+      bodyClasses.toggle('dark-theme')
+      theme = bodyClasses.contains('dark-theme') ? 'dark' : 'light'
+    }
+
+    // update text inside toggler
+    event.target.innerHTML = theme == 'light' ? THEME_LIGHT_TEXT : THEME_DARK_TEXT
+
+    localStorage.setItem('mcinfo-theme', theme)
+  })
+
+  window.onresize = MCInfo.handleResize
+  window.onscroll = MCInfo.handleScroll
+}
+
+
 let theme = ''
 
-// Listen for a click on the button
-btn.addEventListener('click', function(event) {
-  if (prefersDarkScheme.matches) {
-    bodyClasses.toggle('light-theme')
-    theme = bodyClasses.contains('light-theme') ? 'light' : 'dark'
-  } else {
-    bodyClasses.toggle('dark-theme')
-    theme = bodyClasses.contains('dark-theme') ? 'dark' : 'light'
-  }
 
-  // update text inside toggler
-  event.target.innerHTML = theme == 'light' ? THEME_LIGHT_TEXT : THEME_DARK_TEXT
-
-  localStorage.setItem('mcinfo-theme', theme)
-})
 
 MCInfo.initApi = () => {
+  MCInfo.addEventHandlers()
+
   // get external site data
   MCInfo.BG()
   MCInfo.CN()
   MCInfo.GH()
+
+  MCInfo.handleScroll()
+  MCInfo.handleResize()
 }
 
 /************************************************************************
