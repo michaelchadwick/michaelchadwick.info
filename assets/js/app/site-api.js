1b80835b80ae04c0ada9da6eaa8596b8a00b6a28
Michael Chadwick <mike@neb.host>
2023-08-30 10:54:55 -0700

    added check on steam if no games are sent

--- assets/js/app/site-api.js
+++ assets/js/app/site-api.js
@@ -228,13 +228,19 @@ MCInfo.STEAM = function() {
   }).then(data => {
     // console.log('steam api request SUCCESS')
 
-    const game = data['response']['games'][0]
+    const games = data['response']['games']
 
-    const gameTitle = game['name']
-    const gameId = game['appid']
-    const gameUrl = `https://steamcommunity.com/app/${gameId}`
+    if (games.length) {
+      const game = games[0]
 
-    steamLastGamePlayed.innerHTML = `<span>Latest game: <a href="${gameUrl}">${gameTitle}</a></span>`
+      const gameTitle = game['name']
+      const gameId = game['appid']
+      const gameUrl = `https://steamcommunity.com/app/${gameId}`
+
+      steamLastGamePlayed.innerHTML = `<span>Latest game: <a href="${gameUrl}">${gameTitle}</a></span>`
+    } else {
+      steamLastGamePlayed.innerHTML = `<span>Steam failed to respond >:-(</span>`
+    }
 
     if (steamApiData.style.display !== 'block') {
       steamApiData.style.display = 'block'
