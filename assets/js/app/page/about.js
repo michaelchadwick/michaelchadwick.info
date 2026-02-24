cc008817a27d55fe4ed13b786aac6ec3ac015dd6
Michael Chadwick <mike@neb.host>
2026-02-24 13:17:56 -0800

    greetings in several languages on the about page now actually speak the words if window.speechSynthesis exists

--- assets/js/app/page/about.js
+++ assets/js/app/page/about.js
@@ -1,8 +1,26 @@
+const words = document.querySelectorAll('.click-to-talk')
+
+if (words) {
+  words.forEach((word) => word.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    if (window.speechSynthesis) {
+      const synth = window.speechSynthesis
+      const lang = event.target.dataset.lang
+      const text = event.target.innerText
+      const utterThis = new SpeechSynthesisUtterance(text);
+      utterThis.lang = lang
+      utterThis.volume = 0.5
+      synth.speak(utterThis)
+    }
+  }))
+}
+
 const note = document.querySelector('.musical-tone')
 
 if (note) {
-  note.addEventListener('click', (e) => {
-    e.preventDefault()
+  note.addEventListener('click', (event) => {
+    event.preventDefault()
 
     const audioContext = new AudioContext()
     const masterGainNode = audioContext.createGain()
